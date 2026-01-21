4d1b7986665eed2fcc9498d840566df9ff8ea782
AKIRA1921626 <akiraaviutl123456@gmail.com>
2026-01-22 00:58:24 +0900

    add_luafile
    装飾セット用のLuaファイル。
    使いまわしする予定はないが、長くなることが予想できるの初めから分けている。
    現在は書きかけとなる。

---
+++ text_decoration_presets.lua
@@ -0,0 +1,14 @@
+--[[
+装飾セット.anm2などで使う用の.luaファイル。
+プリセットを使用すためだけに使う。
+使いまわしをする予定はないが、長くなると思ったので.luaファイルに分けた。
+]]
+function text_decoration_presets(presets)
+    local effect = obj.effect
+    local various_settings= {
+        effect("")
+    }
+    local vs = various_settings
+
+
+end
